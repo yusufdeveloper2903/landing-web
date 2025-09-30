7dc13e1f1228498f916b527ee92ed8ae47caa838
U2010295 <yusufdeveloper2903@gmail.com>
2025-09-30 15:34:49 +0500

    chore: update TypeScript configuration files and Vite server settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: ["fcf5bf098185.ngrok-free.app"],
+  },
 });
